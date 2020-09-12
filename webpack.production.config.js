1e5651a53f13ac8d6b1574c756379369d3eba52e
unknown <z4ghjdyx@foxmail.com>
2020-09-12 20:30:53 +0800

    feat: 自动清理构建目录

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -7,6 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // https://webpack.js.org/plugins/css-minimizer-webpack-plugin/#root
 // https://cssnano.co/
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 // loader 用于文件转换，将其他文件类型转换成有效的模块，将之添加到依赖图中；接受源文件作为参数，返回转换结果
 // plugin 用于bundel 文件的优化、资源管理、环境变量的注入等；作用于整个构建过程
@@ -63,6 +64,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src/index.html'),
       scriptLoading: 'defer',
